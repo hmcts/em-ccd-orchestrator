a598de19f1c2b2d19f4fb4cefe2c97c6aa4733f3
Linus Norton <linusnorton@gmail.com>
2019-04-18 13:21:28 +0100

    Add functional test for filename

--- src/aat/java/uk/gov/hmcts/reform/em/orchestrator/functional/CcdStitchScenarios.java
+++ src/aat/java/uk/gov/hmcts/reform/em/orchestrator/functional/CcdStitchScenarios.java
@@ -60,4 +60,28 @@ public class CcdStitchScenarios {
         Assert.assertNotNull(path.getString("data.caseBundles[0].value.stitchedDocument.document_url"));
     }
 
+    @Test
+    public void testSpecifyFilename() throws IOException {
+        CcdBundleDTO bundle = testUtil.getTestBundle();
+        bundle.setFileName("my-file-name.pdf");
+
+        String json = mapper.writeValueAsString(new CcdValue<>(bundle));
+        String wrappedJson = String.format("{ \"case_details\":{ \"case_data\":{ \"caseBundles\":[ %s ] } } }", json);
+        System.out.println("JJJ - testPostBundleStitch - request body");
+        System.out.println(wrappedJson);
+
+        Response response = testUtil.authRequest()
+            .header("Content-Type", MediaType.APPLICATION_JSON_VALUE)
+            .body(wrappedJson)
+            .request("POST", Env.getTestUrl() + "/api/stitch-ccd-bundles");
+
+        JsonPath path = response.getBody().jsonPath();
+        System.out.println("JJJ - testPostBundleStitch - response body");
+        System.out.println(path.prettyPrint());
+        Assert.assertEquals(200, response.getStatusCode());
+        Assert.assertEquals("Bundle title", path.getString("data.caseBundles[0].value.title"));
+        Assert.assertEquals("my-file-name.pdf", path.getString("data.caseBundles[0].value.fileName"));
+        Assert.assertNotNull(path.getString("data.caseBundles[0].value.stitchedDocument.document_url"));
+    }
+
 }
