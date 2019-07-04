b39d906f5e21b99e5ea98fb8b9a6e4b273d929d2
Linus Norton <linusnorton@gmail.com>
2019-07-04 09:39:14 +0100

    ensure all properties are checked (#41)

--- src/main/java/uk/gov/hmcts/reform/em/orchestrator/service/caseupdater/JsonNodesVerifier.java
+++ src/main/java/uk/gov/hmcts/reform/em/orchestrator/service/caseupdater/JsonNodesVerifier.java
@@ -7,21 +7,19 @@ public class JsonNodesVerifier {
     private final String[] args;
 
     public JsonNodesVerifier(String... args) {
-        if (args.length % 2 == 1) {
+        if (args.length < 2 || args.length % 2 == 1) {
             throw new IllegalArgumentException("Number of String arguments must be even");
         }
         this.args = args.clone();
     }
 
     public boolean verify(JsonNode jsonNode) {
-        boolean result = false;
         for (int i = 0; i < args.length; i += 2) {
-            result = jsonNode.at(args[i]).asText().equals(args[i + 1]);
-            if (result) {
-                break;
+            if (!jsonNode.at(args[i]).asText().equals(args[i + 1])) {
+                return false;
             }
         }
-        return result;
+        return true;
     }
 
 
