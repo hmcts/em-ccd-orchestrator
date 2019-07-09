364c0bb1964429fb031687776349cb24c2421b89
Linus Norton <linusnorton@gmail.com>
2019-07-09 14:55:06 +0100

    Initial pass at the document set selection configuration

---
+++ src/main/java/uk/gov/hmcts/reform/em/orchestrator/automatedbundling/configuration/BundleConfigurationDocument.java
@@ -0,0 +1,6 @@
+package uk.gov.hmcts.reform.em.orchestrator.automatedbundling.configuration;
+
+public class BundleConfigurationDocument implements BundleConfigurationDocumentSelector {
+    public String name;
+    public String url;
+}
