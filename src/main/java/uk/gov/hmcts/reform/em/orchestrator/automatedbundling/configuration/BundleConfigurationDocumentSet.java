95df7da2c05665557cea4adb832d3a8ab4a3780a
Jeroen Rijks <jeroen-rijks@hotmail.com>
2019-07-16 15:31:02 +0100

    make constructors for yaml parameters

--- src/main/java/uk/gov/hmcts/reform/em/orchestrator/automatedbundling/configuration/BundleConfigurationDocumentSet.java
+++ src/main/java/uk/gov/hmcts/reform/em/orchestrator/automatedbundling/configuration/BundleConfigurationDocumentSet.java
@@ -1,13 +1,30 @@
 package uk.gov.hmcts.reform.em.orchestrator.automatedbundling.configuration;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class BundleConfigurationDocumentSet implements BundleConfigurationDocumentSelector {
-    public String property;
-    public List<BundleConfigurationFilter> filter;
+    public final String property;
+    public final List<BundleConfigurationFilter> filter;
 
     public static class BundleConfigurationFilter {
-        public String property;
-        public String value;
+        public final String property;
+        public final String value;
+
+        public BundleConfigurationFilter(@JsonProperty("property") String property,
+                                         @JsonProperty("value") String value) {
+            this.property = property;
+            this.value = value;
+        }
     }
+
+    public BundleConfigurationDocumentSet(@JsonProperty("property") String property,
+                                          @JsonProperty("filter") List<BundleConfigurationFilter> filter) {
+        this.property= property;
+        this.filter= filter == null ? new ArrayList<>() : filter;
+    }
+
+
 }
