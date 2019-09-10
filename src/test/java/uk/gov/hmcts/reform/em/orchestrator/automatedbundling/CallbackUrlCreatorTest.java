a9a63b38848d7818d36f0518fbba2038920997dd
Pawel Kaczmarek <pawel.kacmarek@solirius.com>
2019-09-10 15:40:22 +0100

    fix

--- src/test/java/uk/gov/hmcts/reform/em/orchestrator/automatedbundling/CallbackUrlCreatorTest.java
+++ src/test/java/uk/gov/hmcts/reform/em/orchestrator/automatedbundling/CallbackUrlCreatorTest.java
@@ -6,13 +6,13 @@ import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class CallbackUrlCreatorTest {
+public class CallbackUrlCreatorTest {
 
     CallbackUrlCreator callbackUrlCreator =
             new CallbackUrlCreator("hostx.com", "https", 1111);
 
     @Test
-    void createCallbackUrl() {
+    public void createCallbackUrl() {
 
         assertEquals(
             "https://hostx.com:1111/api/stitching-complete-callback/12/34/ab8e5c4f-2309-4a47-9ce8-be7f5c114c39",
