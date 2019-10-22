892bb7f65c4c2ebe8146c4ce5c8f15ae96d4ebcf
Pawel Kaczmarek <pawel.kacmarek@solirius.com>
2019-10-22 14:51:11 +0200

    improved docker f-testing

--- src/aat/java/uk/gov/hmcts/reform/em/orchestrator/testutil/CcdHelper.java
+++ src/aat/java/uk/gov/hmcts/reform/em/orchestrator/testutil/CcdHelper.java
@@ -206,7 +206,8 @@ public class CcdHelper {
 
     private String getCallbackUrl() throws Exception {
         if (Env.getTestUrl().contains("localhost")) {
-            return String.format("http://%s:8080", InetAddress.getLocalHost().getHostAddress());
+            return String.format("http://%s:8080", OSValidator.isMac()
+                    ? "host.docker.internal" : InetAddress.getLocalHost().getHostAddress());
         } else {
             return Env.getTestUrl().replaceAll("https", "http");
         }
