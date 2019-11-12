8099ebb3c4b71f0c60670c02e4c230c49e302ff1
Linus Norton <linusnorton@gmail.com>
2019-11-12 15:00:56 +0000

    another blind test

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -104,7 +104,7 @@ module "local_key_vault" {
   env = "${var.env}"
   tenant_id = "${var.tenant_id}"
   object_id = "${var.jenkins_AAD_objectId}"
-  resource_group_name = "${module.app.resource_group_name}"
+  resource_group_name = "${(var.env == "perftest") ? "em-ccd-orchestrator-perftest" : module.app.resource_group_name}"
   product_group_object_id = "5d9cd025-a293-4b97-a0e5-6f43efce02c0"
   common_tags = "${var.common_tags}"
   managed_identity_object_id = "${var.managed_identity_object_id}"
