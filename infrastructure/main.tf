9572169b6c7f7f8e533dc4b9c38ae452b7f8250f
yogesh-hullatti <yogesh.hullatti@hmcts.net>
2019-12-18 11:21:01 +0000

    Updated the vault product name

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -100,7 +100,7 @@ data "azurerm_key_vault" "shared_key_vault" {
 
 module "local_key_vault" {
   source = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
-  product = "${local.app_full_name}"
+  product = "em-ccdorc"
   env = "${var.env}"
   tenant_id = "${var.tenant_id}"
   object_id = "${var.jenkins_AAD_objectId}"
