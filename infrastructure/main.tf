ef81ff41a1a1e75b7e362911a43fbcd784d33820
Linus Norton <linusnorton@gmail.com>
2020-01-09 14:45:09 +0000

    re-enable vault copy

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -111,13 +111,13 @@ module "local_key_vault" {
 }
 
 # Copy s2s key from shared to local vault
-#data "azurerm_key_vault" "local_key_vault" {
-#  name = "${module.local_key_vault.key_vault_name}"
-#  resource_group_name = "${module.local_key_vault.key_vault_name}"
-#}
+data "azurerm_key_vault" "local_key_vault" {
+  name = "${module.local_key_vault.key_vault_name}"
+  resource_group_name = "${module.local_key_vault.key_vault_name}"
+}
 
-#resource "azurerm_key_vault_secret" "local_s2s_key" {
-#  name         = "microservicekey-em-ccd-orchestrator"
-#  value        = "${data.azurerm_key_vault_secret.s2s_key.value}"
-#  key_vault_id = "${data.azurerm_key_vault.local_key_vault.id}"
-#}
+resource "azurerm_key_vault_secret" "local_s2s_key" {
+  name         = "microservicekey-em-ccd-orchestrator"
+  value        = "${data.azurerm_key_vault_secret.s2s_key.value}"
+  key_vault_id = "${data.azurerm_key_vault.local_key_vault.id}"
+}
