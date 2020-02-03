6ffd56b2d2c703f889c4a17a4b5d489070c5e2d2
tomxelliott <tomelliott@Toms-MacBook-Pro.local>
2020-02-03 16:29:20 +0000

    EM-2574: Refactor code to add new bundle at the head of the list of bundles

--- src/main/java/uk/gov/hmcts/reform/em/orchestrator/automatedbundling/AutomatedCaseUpdater.java
+++ src/main/java/uk/gov/hmcts/reform/em/orchestrator/automatedbundling/AutomatedCaseUpdater.java
@@ -65,7 +65,7 @@ public class AutomatedCaseUpdater implements CcdCaseUpdater {
                 ccdCallbackDto.getJwt(),
                 bundle);
 
-        bundles.add(bundleDtoToBundleJson(bundle));
+        bundles.insert(0, bundleDtoToBundleJson(bundle));
 
         return ccdCallbackDto.getCaseData();
     }
