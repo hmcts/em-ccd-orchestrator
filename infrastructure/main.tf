a74284660a873a5077916f54bd775170273b7fbc
Piyush Kumar <piyushk@kainos.com@Piyush-Pool-Laptop.local>
2020-02-03 13:58:00 +0000

    uncomment s2s vault

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -53,8 +53,8 @@ data "azurerm_key_vault" "local_key_vault" {
   resource_group_name = "${module.local_key_vault.key_vault_name}"
 }
 
-# resource "azurerm_key_vault_secret" "local_s2s_key" {
-#   name         = "microservicekey-em-ccd-orchestrator"
-#   value        = "${data.azurerm_key_vault_secret.s2s_key.value}"
-#   key_vault_id = "${data.azurerm_key_vault.local_key_vault.id}"
-# }
+resource "azurerm_key_vault_secret" "local_s2s_key" {
+  name         = "microservicekey-em-ccd-orchestrator"
+  value        = "${data.azurerm_key_vault_secret.s2s_key.value}"
+  key_vault_id = "${data.azurerm_key_vault.local_key_vault.id}"
+}
