b13c9f75afd87174bfa72ed0c21e139bc751fac1
tomxelliott <tomelliott@Toms-MacBook-Pro.local>
2020-02-05 14:45:12 +0000

    EM-2395: Alter email notification field to not use the primitive boolean type

--- src/main/java/uk/gov/hmcts/reform/em/orchestrator/service/dto/CcdBundleDTO.java
+++ src/main/java/uk/gov/hmcts/reform/em/orchestrator/service/dto/CcdBundleDTO.java
@@ -2,6 +2,7 @@ package uk.gov.hmcts.reform.em.orchestrator.service.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.JsonNode;
 import uk.gov.hmcts.reform.em.orchestrator.domain.enumeration.PageNumberFormat;
 
@@ -36,6 +37,8 @@ public class CcdBundleDTO {
     private CcdBundlePaginationStyle paginationStyle = CcdBundlePaginationStyle.off;
     private PageNumberFormat pageNumberFormat = PageNumberFormat.numberOfPages;
     private String stitchingFailureMessage;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private CcdBoolean enableEmailNotification;
 
     public String getId() {
@@ -228,12 +231,16 @@ public class CcdBundleDTO {
     }
 
     @JsonIgnore
-    public void setEnableEmailNotificationAsBoolean(boolean enableEmailNotification) {
-        this.enableEmailNotification = enableEmailNotification ? CcdBoolean.Yes : CcdBoolean.No;
+    public void setEnableEmailNotificationAsBoolean(Boolean enableEmailNotification) {
+        if (enableEmailNotification == null) {
+            this.enableEmailNotification = null;
+        } else {
+            this.enableEmailNotification = enableEmailNotification ? CcdBoolean.Yes : CcdBoolean.No;
+        }
     }
 
     @JsonIgnore
-    public boolean getEnableEmailNotificationAsBoolean() {
-        return enableEmailNotification == CcdBoolean.Yes ? true : false;
+    public Boolean getEnableEmailNotificationAsBoolean() {
+        return enableEmailNotification != null ? enableEmailNotification == CcdBoolean.Yes : null;
     }
 }
--- src/main/java/uk/gov/hmcts/reform/em/orchestrator/stitching/dto/StitchingBundleDTO.java
+++ src/main/java/uk/gov/hmcts/reform/em/orchestrator/stitching/dto/StitchingBundleDTO.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.reform.em.orchestrator.stitching.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.JsonNode;
 import uk.gov.hmcts.reform.em.orchestrator.service.dto.CcdBundlePaginationStyle;
 import uk.gov.hmcts.reform.em.orchestrator.domain.enumeration.PageNumberFormat;
@@ -24,7 +25,9 @@ public class StitchingBundleDTO {
     private boolean hasFolderCoversheets;
     private CcdBundlePaginationStyle paginationStyle = CcdBundlePaginationStyle.off;
     private PageNumberFormat pageNumberFormat = PageNumberFormat.numberOfPages;
-    private boolean enableEmailNotification;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private Boolean enableEmailNotification;
 
     public String getBundleTitle() {
         return bundleTitle;
@@ -130,11 +133,11 @@ public class StitchingBundleDTO {
         this.stitchedDocumentURI = stitchedDocumentURI;
     }
 
-    public boolean getEnableEmailNotification() {
+    public Boolean getEnableEmailNotification() {
         return enableEmailNotification;
     }
 
-    public void setEnableEmailNotification(boolean enableEmailNotification) {
+    public void setEnableEmailNotification(Boolean enableEmailNotification) {
         this.enableEmailNotification = enableEmailNotification;
     }
 }
