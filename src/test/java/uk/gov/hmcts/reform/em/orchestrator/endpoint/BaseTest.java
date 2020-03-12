14a592f24befe52f490ad10251a5158b2842f959
yogesh-hullatti <yogesh.hullatti@hmcts.net>
2020-03-12 18:07:14 +0000

    Updated BaseTest exclusion path for Sonar

--- src/test/java/uk/gov/hmcts/reform/em/orchestrator/endpoint/BaseTest.java
+++ src/test/java/uk/gov/hmcts/reform/em/orchestrator/endpoint/BaseTest.java
@@ -24,7 +24,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 
-public abstract class BaseTest {
+public class BaseTest {
 
     @MockBean
     protected DefaultUpdateCaller defaultUpdateCaller;
