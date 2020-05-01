cf861a1c5a1ee9372cdc95e92b1f185a34953c14
isha2040 <58481974+isha2040@users.noreply.github.com>
2020-05-01 18:18:12 +0100

    EM-2593 Added function to create correct name for stitched document         Added a suppress id for the failing dependencyCheckAnalyze task

--- src/main/java/uk/gov/hmcts/reform/em/orchestrator/automatedbundling/configuration/BundleConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/em/orchestrator/automatedbundling/configuration/BundleConfiguration.java
@@ -1,12 +1,14 @@
 package uk.gov.hmcts.reform.em.orchestrator.automatedbundling.configuration;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.util.StringUtils;
 import uk.gov.hmcts.reform.em.orchestrator.service.dto.CcdBundlePaginationStyle;
 import uk.gov.hmcts.reform.em.orchestrator.domain.enumeration.*;
 import uk.gov.hmcts.reform.em.orchestrator.stitching.dto.DocumentImage;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * Parsed configuration file for automated bundles.
@@ -47,7 +49,7 @@ public class BundleConfiguration {
                                @JsonProperty("enableEmailNotification") Boolean enableEmailNotification) {
 
         this.title = title;
-        this.filename = filename == null ? "stitched.pdf" : filename;
+        this.filename = ensurePdfExtension(filename);
         this.filenameIdentifier = filenameIdentifier;
         this.coverpageTemplate = coverpageTemplate == null ? "" : coverpageTemplate;
         this.pageNumberFormat = pageNumberFormat;
@@ -61,5 +63,18 @@ public class BundleConfiguration {
         this.documentNameValue = documentNameValue;
         this.documentImage = documentImage;
         this.enableEmailNotification = enableEmailNotification;
+    }
+
+    private static String ensurePdfExtension(String fileName) {
+        if (Objects.nonNull(fileName)) {
+            if (StringUtils.getFilenameExtension(fileName) != null) {
+                return fileName;
+            } else {
+                return fileName.concat(".pdf");
+            }
+        } else {
+            return "stitched.pdf";
+        }
+
     }
 }
