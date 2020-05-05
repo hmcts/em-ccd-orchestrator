7f0ad6eabf2c4b7dbe12bdd45edfffbb9fa36343
Kasi Subramaniam <Kasi.Subramaniam@HMCTS.NET>
2020-05-05 23:07:14 +0100

    Revert "EM-2719:Serenity Reporting for CCD Orchestrator"
    This reverts commit 31f58ebc

--- src/aat/java/uk/gov/hmcts/reform/em/orchestrator/functional/BaseTest.java
+++ src/aat/java/uk/gov/hmcts/reform/em/orchestrator/functional/BaseTest.java
@@ -1,18 +1,18 @@
 package uk.gov.hmcts.reform.em.orchestrator.functional;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import net.serenitybdd.junit.spring.integration.SpringIntegrationSerenityRunner;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
 import uk.gov.hmcts.reform.em.EmTestConfig;
 import uk.gov.hmcts.reform.em.orchestrator.testutil.ExtendedCcdHelper;
 import uk.gov.hmcts.reform.em.orchestrator.testutil.TestUtil;
 
 @SpringBootTest(classes = {TestUtil.class, EmTestConfig.class, ExtendedCcdHelper.class})
 @PropertySource(value = "classpath:application.yml")
-@RunWith(SpringIntegrationSerenityRunner.class)
+@RunWith(SpringRunner.class)
 public abstract class BaseTest {
 
     @Autowired
