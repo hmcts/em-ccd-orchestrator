ff104a340068fad5e13563eda0ce3719ea44d808
yogesh-hullatti <yogesh.hullatti@hmcts.net>
2020-06-23 11:27:16 +0100

    Updated Swagger with Auth & Service Auth headers

--- src/main/java/uk/gov/hmcts/reform/em/orchestrator/config/SwaggerConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/em/orchestrator/config/SwaggerConfiguration.java
@@ -4,19 +4,24 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableSwagger2
 @ComponentScan("uk.gov.hmcts.reform.em.orchestrator.rest")
 public class SwaggerConfiguration {
 
-    private static final String apiVersion = "0.0.1";
+    private static final String apiVersion = "1.0.1";
 
     private static final String MODEL_REF_TYPE = "string";
     private static final String PARAMETER_TYPE = "header";
@@ -28,7 +33,30 @@ public class SwaggerConfiguration {
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.regex("/api(.*)"))
                 .build()
-                .apiInfo(apiInfo());
+                .apiInfo(apiInfo())
+                .globalOperationParameters(Arrays.asList(
+                    headerAuthorization(),
+                    headerServiceAuthorization()));
+    }
+
+    private Parameter headerAuthorization() {
+        return new ParameterBuilder()
+            .name("Authorization")
+            .description("Keyword `Bearer` followed by a valid IDAM user token")
+            .modelRef(new ModelRef("string"))
+            .parameterType("header")
+            .required(true)
+            .build();
+    }
+
+    private Parameter headerServiceAuthorization() {
+        return new ParameterBuilder()
+            .name("ServiceAuthorization")
+            .description("Valid Service-to-Service JWT token for a whitelisted micro-service")
+            .modelRef(new ModelRef("string"))
+            .parameterType("header")
+            .required(true)
+            .build();
     }
 
     private ApiInfo apiInfo() {
