603e30ecf53db3a2d2baf6845a9f7b5f2c93d18c
Raj Katla <raj.katla@hmcts.net>
2021-03-16 22:46:07 +0000

    EM-3629 - EM-CCD-Orchestrator: Fix ignored test cases

--- src/aat/java/uk/gov/hmcts/reform/em/orchestrator/functional/CcdPrehookScenarios.java
+++ src/aat/java/uk/gov/hmcts/reform/em/orchestrator/functional/CcdPrehookScenarios.java
@@ -1,9 +1,5 @@
 package uk.gov.hmcts.reform.em.orchestrator.functional;
 
-import io.restassured.path.json.JsonPath;
-import io.restassured.response.Response;
-import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import uk.gov.hmcts.reform.em.test.retry.RetryRule;
@@ -12,9 +8,9 @@ import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
 
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
-@Ignore
 public class CcdPrehookScenarios extends BaseTest {
 
     private final File jsonFile = new File(ClassLoader.getSystemResource("prehook-case.json").getPath());
@@ -24,21 +20,22 @@ public class CcdPrehookScenarios extends BaseTest {
 
     @Test
     public void testPostBundleStitch() {
-        Response response =
-                testUtil
-                        .authRequest()
-                        .baseUri(testUtil.getTestUrl())
-                        .contentType(APPLICATION_JSON_VALUE)
-                        .body(jsonFile)
-                        .post("/api/new-bundle");
-
-        Assert.assertEquals(200, response.getStatusCode());
-        Assert.assertEquals("New Bundle", response.getBody().jsonPath().getString("data.caseBundles[0].value.title"));
+        testUtil
+                .authRequest()
+                .baseUri(testUtil.getTestUrl())
+                .contentType(APPLICATION_JSON_VALUE)
+                .body(jsonFile)
+                .post("/api/new-bundle")
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .body("data.caseBundles[0].value.title", equalTo("New bundle"))
+                .log().all();
     }
 
     @Test
     public void testEndToEnd() throws IOException {
-        HashMap caseData =
+        final HashMap<String, String> caseData =
                 testUtil
                         .authRequest()
                         .baseUri(testUtil.getTestUrl())
@@ -50,27 +47,25 @@ public class CcdPrehookScenarios extends BaseTest {
                         .get("data");
 
         // pretend the user has modified some fields
-        String uploadedDocUri = testUtil.uploadDocument();
-        String caseJson = mapper.writeValueAsString(caseData)
+        final String uploadedDocUri = testUtil.uploadDocument();
+        final String caseJson = mapper.writeValueAsString(caseData)
                 .replace("New Bundle", "Bundle title")
                 .replace("hasCoversheets\":\"Yes", "hasCoversheets\":\"No")
                 .replace("http://dm-store:8080/documents/05647df3-094c-45a3-b667-2a6f1bf3d088", uploadedDocUri);
 
-        String request = String.format("{ \"case_details\":{ \"case_data\": %s } } }", caseJson);
+        final String request = String.format("{ \"case_details\":{ \"case_data\": %s } } }", caseJson);
 
-        Response response =
-                testUtil
-                        .authRequest()
-                        .baseUri(testUtil.getTestUrl())
-                        .contentType(APPLICATION_JSON_VALUE)
-                        .body(request)
-                        .post("/api/stitch-ccd-bundles");
-
-        JsonPath path = response.getBody().jsonPath();
-        Assert.assertEquals(200, response.getStatusCode());
-        Assert.assertEquals("Bundle title", path.getString("data.caseBundles[0].value.title"));
-        Assert.assertEquals("No", path.getString("data.caseBundles[0].value.hasCoversheets"));
-        Assert.assertNotNull(path.getString("data.caseBundles[0].value.stitchedDocument.document_url"));
-
+        testUtil
+                .authRequest()
+                .baseUri(testUtil.getTestUrl())
+                .contentType(APPLICATION_JSON_VALUE)
+                .body(request)
+                .post("/api/stitch-ccd-bundles")
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .body("data.caseBundles[0].value.title", equalTo("New bundle"))
+                .body("data.caseBundles[0].value.hasCoversheets", equalTo("No"))
+                .log().all();
     }
 }
