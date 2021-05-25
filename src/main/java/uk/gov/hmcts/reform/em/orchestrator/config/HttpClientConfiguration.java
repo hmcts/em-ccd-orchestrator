a5e1958f3d6648818a2e73ff99804c94875c43a5
yogesh-hullatti <yogesh.hullatti@hmcts.net>
2021-05-25 14:03:41 +0100

    Increased httpClient timeout

--- src/main/java/uk/gov/hmcts/reform/em/orchestrator/config/HttpClientConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/em/orchestrator/config/HttpClientConfiguration.java
@@ -1,16 +1,25 @@
 package uk.gov.hmcts.reform.em.orchestrator.config;
 
 import okhttp3.OkHttpClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class HttpClientConfiguration {
 
+    @Value("${httpclient.call-timeout}")
+    private int timeout;
+
     @Bean
     public OkHttpClient okHttpClient() {
         return new OkHttpClient.Builder()
-                .build();
+            .connectTimeout(timeout, TimeUnit.SECONDS)
+            .readTimeout(timeout, TimeUnit.SECONDS)
+            .writeTimeout(timeout, TimeUnit.SECONDS)
+            .build();
 
     }
 
