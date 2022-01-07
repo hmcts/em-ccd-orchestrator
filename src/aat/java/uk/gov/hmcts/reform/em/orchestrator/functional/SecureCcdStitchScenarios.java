a82736e545a5410bf0b0131a8b4d2a334d15b091
yogesh-hullatti <yogesh.hullatti@hmcts.net>
2022-01-07 11:14:54 +0000

    Test PR to check 9.6 Db on AAT

--- src/aat/java/uk/gov/hmcts/reform/em/orchestrator/functional/SecureCcdStitchScenarios.java
+++ src/aat/java/uk/gov/hmcts/reform/em/orchestrator/functional/SecureCcdStitchScenarios.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.reform.em.orchestrator.functional;
 
 import io.restassured.response.ValidatableResponse;
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import uk.gov.hmcts.reform.em.orchestrator.config.Constants;
@@ -13,6 +14,7 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
+@Ignore
 public class SecureCcdStitchScenarios extends BaseTest {
 
     @Rule
