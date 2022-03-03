500c44218abff3fcb4bb2a9f8900ba5931787e68
yogesh-hullatti <yogesh.hullatti@hmcts.net>
2022-03-03 16:59:22 +0000

    Upgraded SpringBoot version to 2.6.4

--- src/main/java/uk/gov/hmcts/reform/em/orchestrator/Application.java
+++ src/main/java/uk/gov/hmcts/reform/em/orchestrator/Application.java
@@ -6,10 +6,8 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
-@EnableSwagger2
 @EnableScheduling
 @EnableAspectJAutoProxy(proxyTargetClass = true)
 public class Application {
--- src/main/java/uk/gov/hmcts/reform/em/orchestrator/config/SwaggerConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/em/orchestrator/config/SwaggerConfiguration.java
@@ -1,92 +1,22 @@
 package uk.gov.hmcts.reform.em.orchestrator.config;
 
-import org.springframework.context.annotation.Bean;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.ParameterBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.schema.ModelRef;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Parameter;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import java.util.Arrays;
 
 @Configuration
-@EnableSwagger2
-@ComponentScan("uk.gov.hmcts.reform.em.orchestrator.rest")
+@ComponentScan("uk.gov.hmcts.reform.em.orchestrator.endpoint")
 public class SwaggerConfiguration {
 
-    private static final String apiVersion = "1.0.1";
-
-    private static final String MODEL_REF_TYPE = "string";
-    private static final String PARAMETER_TYPE = "header";
-
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.regex("/api(.*)"))
-                .build()
-                .apiInfo(apiInfo())
-                .globalOperationParameters(Arrays.asList(
-                    headerAuthorization(),
-                    headerServiceAuthorization(),
-                    requestCaseTypeId(),
-                    requestJurisdictionId()));
-    }
-
-    private Parameter headerAuthorization() {
-        return new ParameterBuilder()
-            .name("Authorization")
-            .description("Keyword `Bearer` followed by a valid IDAM user token")
-            .modelRef(new ModelRef("string"))
-            .parameterType("header")
-            .required(true)
-            .build();
-    }
-
-    private Parameter headerServiceAuthorization() {
-        return new ParameterBuilder()
-            .name("ServiceAuthorization")
-            .description("Valid Service-to-Service JWT token for a whitelisted micro-service")
-            .modelRef(new ModelRef("string"))
-            .parameterType("header")
-            .required(true)
-            .build();
-    }
-
-    private Parameter requestCaseTypeId() {
-        return new ParameterBuilder()
-            .name("caseTypeId")
-            .description("Valid caseTypeId required for documents to use CDAM")
-            .modelRef(new ModelRef("string"))
-            .parameterType("request")
-            .required(false)
-            .build();
-    }
-
-    private Parameter requestJurisdictionId() {
-        return new ParameterBuilder()
-            .name("jurisdictionId")
-            .description("Valid jurisdictionId required for documents to use CDAM")
-            .modelRef(new ModelRef("string"))
-            .parameterType("request")
-            .required(false)
-            .build();
-    }
-
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("EM CCD Orchestrator")
-                .description("Orchestrates callbacks from CCD relating to management and stitching of bundles.\n "
-                    + "caseTypeId & jurisdictionId are required attributes for Documents to use CDAM.")
-                .version(apiVersion)
-                .build();
+    public OpenAPI api() {
+        return new OpenAPI()
+                .info(
+                        new Info().title("EM CCD Orchestrator")
+                                .description("Orchestrates callbacks from CCD relating to management and "
+                                        + "stitching of bundles.\n caseTypeId & jurisdictionId "
+                                        + "are required attributes for Documents to use CDAM.")
+                                .version("v1.0.1")
+                );
     }
 }
