3b0ceb99ae652fe0dcdb32f66eecc34b560b5adf
yogesh-hullatti <yogesh.hullatti@hmcts.net>
2022-03-30 14:22:15 +0100

    Updated azuremr and terraform version

--- infrastructure/state.tf
+++ infrastructure/state.tf
@@ -4,11 +4,11 @@ terraform {
   required_providers {
     azuread = {
       source  = "hashicorp/azuread"
-      version = "1.6.0"
+      version = "2.3.0"
     }
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 2.35"
+      version = "~> 3.0.2"
     }
   }
 }
