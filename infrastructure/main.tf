99de5114f20e72dc91609e3fb44892e55d4ac2f0
mdayican <mustafa.dayican@hmcts.net>
2022-09-14 10:45:22 +0100

    EM-4227 copy app-insight connection from rpa vault

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -64,3 +64,14 @@ resource "azurerm_key_vault_secret" "appinsights_key" {
   value        = data.azurerm_application_insights.appinsights.instrumentation_key
   key_vault_id = module.local_key_vault.key_vault_id
 }
+
+data "azurerm_key_vault_secret" "app_insights_connection_string" {
+  name         = "app-insights-connection-string"
+  key_vault_id = data.azurerm_key_vault.product.id
+}
+
+resource "azurerm_key_vault_secret" "local_app_insights_connection_string" {
+  name         = "app-insights-connection-string"
+  value        = data.azurerm_key_vault_secret.app_insights_connection_string.value
+  key_vault_id = data.azurerm_key_vault.local_key_vault.id
+}
