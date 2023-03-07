e93a84ae4bc8ae599afc1f7a110056773acdfc14
yogesh-hullatti <yogesh.hullatti@hmcts.net>
2023-03-07 15:27:28 +0000

    Updated Stitching URL for PR

--- src/aat/java/uk/gov/hmcts/reform/em/orchestrator/functional/SecureAutomatedBundlingWithCallbacks.java
+++ src/aat/java/uk/gov/hmcts/reform/em/orchestrator/functional/SecureAutomatedBundlingWithCallbacks.java
@@ -10,7 +10,6 @@ import uk.gov.hmcts.reform.em.test.retry.RetryRule;
 import java.util.ArrayList;
 import java.util.List;
 
-@Ignore("Need to investigate further")
 public class SecureAutomatedBundlingWithCallbacks extends BaseTest {
 
     private final Logger logger = LoggerFactory.getLogger(SecureAutomatedBundlingWithCallbacks.class);
