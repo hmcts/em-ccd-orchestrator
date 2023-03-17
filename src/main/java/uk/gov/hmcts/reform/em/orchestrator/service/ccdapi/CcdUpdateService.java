893e14a441200bdf210aad444da894d0f07c0e83
MUSTAFA <mustafa.dayican1@hmcts.net>
2023-03-17 07:40:28 +0000

    Em-5067 refactor ccd update (#1108)

---
+++ src/main/java/uk/gov/hmcts/reform/em/orchestrator/service/ccdapi/CcdUpdateService.java
@@ -0,0 +1,62 @@
+package uk.gov.hmcts.reform.em.orchestrator.service.ccdapi;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
+import uk.gov.hmcts.reform.ccd.client.CoreCaseDataApi;
+import uk.gov.hmcts.reform.ccd.client.model.CaseDataContent;
+import uk.gov.hmcts.reform.ccd.client.model.CaseResource;
+import uk.gov.hmcts.reform.ccd.client.model.Event;
+import uk.gov.hmcts.reform.ccd.client.model.StartEventResponse;
+import uk.gov.hmcts.reform.em.orchestrator.service.ccdcallbackhandler.CcdCallbackDto;
+import uk.gov.hmcts.reform.em.orchestrator.service.ccdcallbackhandler.CcdCallbackDtoCreator;
+
+@Service
+public class CcdUpdateService {
+
+    private final Logger log = LoggerFactory.getLogger(CcdUpdateService.class);
+    private final CoreCaseDataApi coreCaseDataApi;
+    private final AuthTokenGenerator authTokenGenerator;
+    private final CcdCallbackDtoCreator ccdCallbackDtoCreator;
+
+    public CcdUpdateService(
+            CoreCaseDataApi coreCaseDataApi,
+            AuthTokenGenerator authTokenGenerator,
+            CcdCallbackDtoCreator ccdCallbackDtoCreator
+    ) {
+        this.coreCaseDataApi = coreCaseDataApi;
+        this.authTokenGenerator = authTokenGenerator;
+        this.ccdCallbackDtoCreator = ccdCallbackDtoCreator;
+    }
+
+    public CcdCallbackDto startCcdEvent(String caseId, String triggerId, String jwt) {
+
+        StartEventResponse startEventResponse =
+                coreCaseDataApi.startEvent(
+                        jwt,
+                        authTokenGenerator.generate(),
+                        caseId,
+                        triggerId
+                );
+
+        return ccdCallbackDtoCreator.createDto(
+                "caseBundles",
+                jwt,
+                startEventResponse);
+
+    }
+
+    public void submitCcdEvent(String caseId, String jwt, CcdCallbackDto ccdCallbackDto) {
+        CaseDataContent caseDataContent = CaseDataContent.builder()
+                .data(ccdCallbackDto.getCaseData())
+                .event(Event.builder().id(ccdCallbackDto.getEventId()).build())
+                .eventToken(ccdCallbackDto.getEventToken())
+                .build();
+
+        CaseResource caseDetails = coreCaseDataApi
+                .createEvent(jwt, authTokenGenerator.generate(), caseId, caseDataContent
+                );
+        log.info("CCD event submitted for reference  {} ", caseDetails.getReference());
+    }
+}
--- src/main/java/uk/gov/hmcts/reform/em/orchestrator/service/orchestratorcallbackhandler/StitchingCompleteCallbackService.java
+++ src/main/java/uk/gov/hmcts/reform/em/orchestrator/service/orchestratorcallbackhandler/StitchingCompleteCallbackService.java
@@ -2,44 +2,34 @@ package uk.gov.hmcts.reform.em.orchestrator.service.orchestratorcallbackhandler;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import uk.gov.hmcts.reform.em.orchestrator.service.ccdapi.CcdDataApiCaseUpdater;
-import uk.gov.hmcts.reform.em.orchestrator.service.ccdapi.CcdDataApiEventCreator;
+import uk.gov.hmcts.reform.em.orchestrator.service.ccdapi.CcdUpdateService;
 import uk.gov.hmcts.reform.em.orchestrator.service.ccdcallbackhandler.CcdCallbackDto;
 
 @Service
 @Transactional
 public class StitchingCompleteCallbackService {
 
-    private final CcdDataApiEventCreator ccdDataApiEventCreator;
-    private final CcdDataApiCaseUpdater ccdDataApiCaseUpdater;
+    private final CcdUpdateService ccdUpdateService;
     private final CcdCallbackBundleUpdater ccdCallbackBundleUpdater;
 
-    public StitchingCompleteCallbackService(CcdDataApiEventCreator ccdDataApiEventCreator,
-                                            CcdDataApiCaseUpdater ccdDataApiCaseUpdater,
-                                            CcdCallbackBundleUpdater ccdCallbackBundleUpdater) {
-        this.ccdDataApiEventCreator = ccdDataApiEventCreator;
-        this.ccdDataApiCaseUpdater = ccdDataApiCaseUpdater;
+    public StitchingCompleteCallbackService(
+            CcdUpdateService ccdUpdateService,
+            CcdCallbackBundleUpdater ccdCallbackBundleUpdater
+    ) {
+        this.ccdUpdateService = ccdUpdateService;
         this.ccdCallbackBundleUpdater = ccdCallbackBundleUpdater;
     }
 
     public void handleCallback(StitchingCompleteCallbackDto stitchingCompleteCallbackDto) {
-
         CcdCallbackDto ccdCallbackDto = null;
 
-        try {
+        ccdCallbackDto = ccdUpdateService.startCcdEvent(stitchingCompleteCallbackDto.getCaseId(),
+                stitchingCompleteCallbackDto.getTriggerId(),
+                stitchingCompleteCallbackDto.getJwt());
 
-            ccdCallbackDto = ccdDataApiEventCreator.executeTrigger(stitchingCompleteCallbackDto.getCaseId(),
-                    stitchingCompleteCallbackDto.getTriggerId(),
-                    stitchingCompleteCallbackDto.getJwt());
+        ccdCallbackBundleUpdater.updateBundle(ccdCallbackDto, stitchingCompleteCallbackDto);
 
-            ccdCallbackBundleUpdater.updateBundle(ccdCallbackDto, stitchingCompleteCallbackDto);
-
-        } finally {
-            if (ccdCallbackDto != null) {
-                ccdDataApiCaseUpdater.executeUpdate(ccdCallbackDto, stitchingCompleteCallbackDto.getJwt());
-            }
-        }
-
+        ccdUpdateService.submitCcdEvent(stitchingCompleteCallbackDto.getCaseId(), stitchingCompleteCallbackDto.getJwt(), ccdCallbackDto);
     }
 
 }
