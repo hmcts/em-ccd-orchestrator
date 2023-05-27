29117c2df2ac4903914c07189838305ef437d54b
day67 <mustafa.dayican1@hmcts.net>
2023-05-27 23:40:48 +0100

    remove exclude

--- src/main/java/uk/gov/hmcts/reform/em/orchestrator/Application.java
+++ src/main/java/uk/gov/hmcts/reform/em/orchestrator/Application.java
@@ -2,11 +2,9 @@ package uk.gov.hmcts.reform.em.orchestrator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@SpringBootApplication
 @EnableScheduling
 public class Application {
     public static void main(String[] args) {
