c7a232c639342359ac0932fa6f0032fdd3eec9c8
mdayican <mdayican@gmail.com>
2023-05-30 16:05:29 +0100

    fix testing

--- src/main/java/uk/gov/hmcts/reform/em/orchestrator/Application.java
+++ src/main/java/uk/gov/hmcts/reform/em/orchestrator/Application.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(scanBasePackages = {"uk.gov.hmcts.reform.em.orchestrator",
-        "uk.gov.hmcts.reform.ccd.document.am",
         "uk.gov.hmcts.reform.authorisation",
         "uk.gov.hmcts.reform.ccd.client"}
 )
