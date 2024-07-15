dd93abc7c50cd4badecb435a15f21984e63d7a54
mdayican <mdayican@gmail.com>
2024-07-15 10:25:37 +0100

    test on pr

--- src/contractTest/java/uk/gov/hmcts/reform/em/orchestrator/controller/BundleProviderTest.java
+++ src/contractTest/java/uk/gov/hmcts/reform/em/orchestrator/controller/BundleProviderTest.java
@@ -5,7 +5,6 @@ import au.com.dius.pact.provider.junit5.PactVerificationInvocationContextProvide
 import au.com.dius.pact.provider.junitsupport.IgnoreNoPactsToVerify;
 import au.com.dius.pact.provider.junitsupport.Provider;
 import au.com.dius.pact.provider.junitsupport.loader.PactBroker;
-import au.com.dius.pact.provider.junitsupport.loader.VersionSelector;
 import au.com.dius.pact.provider.spring.junit5.MockMvcTestTarget;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.TestTemplate;
@@ -23,8 +22,7 @@ import uk.gov.hmcts.reform.em.orchestrator.service.caseupdater.DefaultUpdateCall
 @Provider("em_ccd_orc_bundling")
 @PactBroker(scheme = "${PACT_BROKER_SCHEME:http}",
         host = "${PACT_BROKER_URL:localhost}",
-        port = "${PACT_BROKER_PORT:80}",
-        consumerVersionSelectors = {@VersionSelector(tag = "master")})
+        port = "${PACT_BROKER_PORT:80}")
 @IgnoreNoPactsToVerify
 @AutoConfigureMockMvc(addFilters = false)
 @ExtendWith(SpringExtension.class)
