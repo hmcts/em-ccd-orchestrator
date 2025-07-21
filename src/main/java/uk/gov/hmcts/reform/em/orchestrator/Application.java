c6d4ed529e9302b9a0d1d2fe2eea20ba397c058b
michaeljacob <Michael.Jacob@HMCTS.NET>
2025-07-21 13:05:16 +0100

    update validateyaml task

--- src/main/java/uk/gov/hmcts/reform/em/orchestrator/Application.java
+++ src/main/java/uk/gov/hmcts/reform/em/orchestrator/Application.java
@@ -1,17 +1,41 @@
 package uk.gov.hmcts.reform.em.orchestrator;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import uk.gov.hmcts.reform.em.orchestrator.automatedbundling.command.ValidateYamlCommand;
+
+import java.util.Arrays;
 
 @SpringBootApplication(scanBasePackages = {"uk.gov.hmcts.reform.em.orchestrator",
     "uk.gov.hmcts.reform.authorisation",
-    "uk.gov.hmcts.reform.ccd.client"}
+    "uk.gov.hmcts.reform.ccd.client"},
+    exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class}
 )
-@SuppressWarnings("HideUtilityClassConstructor") // Spring needs a constructor, its not a utility class
 @EnableScheduling
-public class Application {
+public class Application implements CommandLineRunner {
+
+    private final ValidateYamlCommand validateYamlCommand;
+
+    public Application(ValidateYamlCommand validateYamlCommand) {
+        this.validateYamlCommand = validateYamlCommand;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
+    @Override
+    public void run(String... args) {
+        if (Arrays.asList(args).contains("--validate")) {
+            if (!validateYamlCommand.run()) {
+                System.exit(-1);
+            }
+            System.exit(0);
+        }
+    }
+
 }
