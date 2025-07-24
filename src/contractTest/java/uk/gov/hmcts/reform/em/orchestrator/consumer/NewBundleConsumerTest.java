7a4346e89fe096ddee7767855cc8562ad784056f
MichaelJacob21012 <88201349+MichaelJacob21012@users.noreply.github.com>
2025-07-24 13:09:58 +0100

    add pact test (#1988)
    * add pact test
    
    * fix annotation

---
+++ src/contractTest/java/uk/gov/hmcts/reform/em/orchestrator/consumer/NewBundleConsumerTest.java
@@ -0,0 +1,79 @@
+package uk.gov.hmcts.reform.em.orchestrator.consumer;
+
+import au.com.dius.pact.consumer.MockServer;
+import au.com.dius.pact.consumer.dsl.DslPart;
+import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
+import au.com.dius.pact.consumer.junit5.PactTestFor;
+import au.com.dius.pact.core.model.V4Pact;
+import au.com.dius.pact.core.model.annotations.Pact;
+import io.restassured.http.ContentType;
+import net.serenitybdd.rest.SerenityRest;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+
+import java.util.UUID;
+
+import static au.com.dius.pact.consumer.dsl.LambdaDsl.newJsonBody;
+
+class NewBundleConsumerTest extends BaseConsumerTest {
+
+    private static final String NEW_BUNDLE_PROVIDER_NAME = "em_orchestrator_new_bundle_provider";
+    private static final String NEW_BUNDLE_API_PATH = "/api/new-bundle";
+
+    @Pact(provider = NEW_BUNDLE_PROVIDER_NAME, consumer = ORCHESTRATOR_CONSUMER)
+    public V4Pact prepareNewBundle200(PactDslWithProvider builder) {
+        return builder
+            .given("a request to prepare a new bundle is successful")
+            .uponReceiving("A POST request to prepare a new bundle")
+            .path(NEW_BUNDLE_API_PATH)
+            .method(HttpMethod.POST.toString())
+            .headers(getHeaders())
+            .body(createNewBundleRequestDsl())
+            .willRespondWith()
+            .status(HttpStatus.OK.value())
+            .body(createPrepareNewBundleResponseDsl())
+            .toPact(V4Pact.class);
+    }
+
+    @Test
+    @PactTestFor(pactMethod = "prepareNewBundle200", providerName = NEW_BUNDLE_PROVIDER_NAME)
+    void testPrepareNewBundle200(MockServer mockServer) {
+        SerenityRest
+            .given()
+            .headers(getHeaders())
+            .contentType(ContentType.JSON)
+            .body(createNewBundleRequestDsl().getBody().toString())
+            .post(mockServer.getUrl() + NEW_BUNDLE_API_PATH)
+            .then()
+            .statusCode(HttpStatus.OK.value());
+    }
+
+    private DslPart createNewBundleRequestDsl() {
+        return newJsonBody(body -> {
+            body.object("case_details", details -> {
+                details.object("case_data", data -> {
+                    data.stringType("caseTitle", "My Test Case");
+                    data.eachLike("caseBundles", bundle ->
+                        bundle.object("value", value -> {
+                            value.uuid("id", UUID.fromString("a585a03b-a521-443b-826c-9411ebd44733"));
+                            value.stringType("title", "Test Bundle");
+                        })
+                    );
+                });
+            });
+            body.stringType("event_id", "createBundle");
+        }).build();
+    }
+
+    private DslPart createPrepareNewBundleResponseDsl() {
+        return newJsonBody(body -> {
+            body.object("data", data -> {
+                data.stringType("bundleTitle", "Bundle");
+            });
+            body.array("errors", arr -> {});
+            body.array("warnings", arr -> {});
+            body.numberType("documentTaskId", 12345L);
+        }).build();
+    }
+}
---
+++ src/contractTest/java/uk/gov/hmcts/reform/em/orchestrator/provider/BaseProviderTest.java
@@ -0,0 +1,56 @@
+package uk.gov.hmcts.reform.em.orchestrator.provider;
+
+import au.com.dius.pact.provider.junit5.PactVerificationContext;
+import au.com.dius.pact.provider.junit5.PactVerificationInvocationContextProvider;
+import au.com.dius.pact.provider.junitsupport.IgnoreNoPactsToVerify;
+import au.com.dius.pact.provider.junitsupport.loader.PactBroker;
+import au.com.dius.pact.provider.junitsupport.loader.PactBrokerConsumerVersionSelectors;
+import au.com.dius.pact.provider.junitsupport.loader.SelectorBuilder;
+import au.com.dius.pact.provider.spring.junit5.MockMvcTestTarget;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.TestTemplate;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
+
+@IgnoreNoPactsToVerify
+@AutoConfigureMockMvc(addFilters = false)
+//@PactFolder("pacts")
+@PactBroker(
+    url = "${PACT_BROKER_FULL_URL:http://localhost:80}",
+    providerBranch = "${pact.provider.branch}"
+)
+public abstract class BaseProviderTest {
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @BeforeEach
+    void setupPactVerification(PactVerificationContext context) {
+        MockMvcTestTarget testTarget = new MockMvcTestTarget(mockMvc);
+        testTarget.setControllers(getControllersUnderTest());
+
+        if (context != null) {
+            context.setTarget(testTarget);
+        }
+    }
+
+    protected abstract Object[] getControllersUnderTest();
+
+    @TestTemplate
+    @ExtendWith(PactVerificationInvocationContextProvider.class)
+    void pactVerificationTestTemplate(PactVerificationContext context) {
+        if (context != null) {
+            context.verifyInteraction();
+        }
+    }
+
+    @PactBrokerConsumerVersionSelectors
+    public static SelectorBuilder consumerVersionSelectors() {
+        return new SelectorBuilder()
+            .matchingBranch()
+            .mainBranch()
+            .deployedOrReleased();
+    }
+}
