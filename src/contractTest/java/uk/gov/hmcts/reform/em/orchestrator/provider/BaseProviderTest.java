71832337359af2d8f3f593fa6100a0a9b0c89a0a
michaeljacob <Michael.Jacob@HMCTS.NET>
2026-09-10 14:30:05 +0100

    enable wip pacts

--- src/contractTest/java/uk/gov/hmcts/reform/em/orchestrator/provider/BaseProviderTest.java
+++ src/contractTest/java/uk/gov/hmcts/reform/em/orchestrator/provider/BaseProviderTest.java
@@ -20,7 +20,8 @@ import org.springframework.test.web.servlet.MockMvc;
 @PactBroker(
     url = "${PACT_BROKER_FULL_URL:http://localhost:80}",
     providerBranch = "${pact.provider.branch}",
-    enablePendingPacts = "${pactbroker.enablePending:true}"
+    enablePendingPacts = "${pactbroker.enablePending:true}",
+    includeWipPactsSince = "${pactbroker.includeWipPactsSince:}"
 )
 public abstract class BaseProviderTest {
 
