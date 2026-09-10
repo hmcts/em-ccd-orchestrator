fd0d4479420156010ab07a163b846bf58ae38e13
yogesh-hullatti <yogesh.hullatti@hmcts.net>
2026-09-10 16:49:48 +0100

    Implement review comment

--- src/test/java/uk/gov/hmcts/reform/em/orchestrator/automatedbundling/BundleFactoryTest.java
+++ src/test/java/uk/gov/hmcts/reform/em/orchestrator/automatedbundling/BundleFactoryTest.java
@@ -32,7 +32,6 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
-
 class BundleFactoryTest {
 
     private final BundleFactory factory = new BundleFactory();
